e34fe48c454373b230f455947120d6274812ab20
ADMS <ankitfn876@gmail.com>
2025-11-18 22:25:07 +0530

    up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -32,7 +32,7 @@ export class HomeComponent {
 email:string='realkryddalivet@gmail.com';
   slides = [
     { id: 1, src: 'assets/blends/Blend.jpeg', alt: 'Blend spices' },
-    { id: 2, src: 'assets/blends/tea1.png', alt: 'Tea' },
+    { id: 2, src: 'assets/blends/tea.jpg', alt: 'Tea' },
     { id: 3, src: 'assets/blends/paneer.jpg', alt: 'Paneer' }
   ];
   ngOnInit(): void {
